2243d66be9c158819bf8dde5cd3dbce989c7e321
Wrightl71559 <60583421+Wrightl71559@users.noreply.github.com>
2020-04-21 18:00:38 +1200

    c0mpoent4

---
+++ 04_statement_generator.py
@@ -0,0 +1,11 @@
+# component 4 - generates formatted statements
+# create a statement generating function
+# implement function into main code
+
+# statement generator function
+def hl_statement(statement, char):
+    print()
+    print(char*len(statement))
+    print(statement)
+    print(char*len(statement))
+    print
